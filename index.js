de09634a9ee0dadef0fdec8545aeebd20e29e88a
Jesper Thörnberg <jesper.thornberg@me.com>
2018-09-26 02:46:24 +0200

    Simplify index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ var mongoose = require('mongoose');
 /* MLAB */
 /* 'mongodb://root:root@ds141068.mlab.com:41068/trollycommute'  */  
 mongoose.connect('mongodb://localhost/trollycommute')
-.then(()=>console.log('Esablished connection to mLab...'))
-.catch((err)=>res.send(err))
+.then(() => console.log('Esablished connection to mLab...'))
+.catch((err) => res.send(err))
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -26,9 +26,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('port', port);
 app.use(express.static(__dirname + '/client/build'));
 
-app.listen(app.get('port'), function(){
-	console.log('Running @ ' + app.get('port'))
-});
+app.listen(app.get('port'), () => console.log('Running @ ' + app.get('port')));
 
 // Main route
 app.get('/', function(req, res) {
@@ -36,51 +34,32 @@ app.get('/', function(req, res) {
 	.sendFile(path.join(__dirname, '/client/build/index.html'))
 });
 
-
-
 // Fetches stored stoplocations from mongodb
 app.post('/api/stops', function(req, res){
 	api.GetMatchingStops(req.body.search)
-	.then((data)=>{
-		res.json({searchResponse: data});
-	})
-	.catch((err)=>res.json(err));
+	.then((data) => res.json({searchResponse: data}))
+	.catch((err) => res.json(err));
 })
 
 // Fetches trip data from remote
 app.post('/api/trips', function(req, res){
 	api.GetTripFromSearch(req.body.origin, req.body.destination)
-	.then((data)=>{
-		res.json(data)
-	})
-	.catch((err)=>res.send(err))
+	.then((data) => res.json(data))
+	.catch((err) => res.send(err))
 })
 
 // Updates the stored stoplocations with new ones from vasttrafik
 app.get('/api/stoplocations', function(req, res) {
 	api.GetAllStopLocations()
-	.then((result)=>{
-		return res.json(result);
-	})
-	.catch((err)=>{
-		console.log(err);
-		res.send({error: err})
-	})
+	.then((result) => res.json(result))
+	.catch((err) => res.send({error: err}))
 })
 
 app.get('/api/update/stoplocations', function(req, res){
 	StopLocation.remove()
-	.then(()=>{
-		return api.FetchStopLocations();
-	})
-	.then((res)=>{
-		return api.InsertStopLocations(res);
-	})
-	.then(()=>{
-		console.log('Done')
-		res.json({data: 'Success'});
-	})
-	.catch((err)=>{
-		res.send({error: 'Failed inserting', stack: err})
-	})
+	.then(() => api.FetchStopLocations())
+	.then((res) => api.InsertStopLocations(res))
+	.then(() => res.json({data: 'Success'}))
+	.then(() => console.log('Done'))
+	.catch((err) => res.send({error: 'Failed inserting', stack: err}))
 })
