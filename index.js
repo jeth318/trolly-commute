5c35282fbefac099d93f4796ded9858ed5d6df1f
Jesper Thörnberg <jesper.thornberg@me.com>
2019-10-22 12:25:55 +0200

    Change port to 3001

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-const port = process.env.PORT || '5000';
+const port = 3001;;
 const https = require('https');
 const exec = require('child_process').exec;
 
