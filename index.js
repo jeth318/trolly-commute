0c1f5750b5e5a2f418d9600a90d1899853628e39
Jesper Thörnberg <jesper.thornberg@me.com>
2019-10-24 17:31:17 +0200

    Remove deployment process

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3001;
-const runDeployment = require('./deployWorker');
 
 // Middleware
 app.use(bodyParser.json({limit: '50mb'}));
@@ -16,16 +15,4 @@ app.listen(app.get('port'), () => console.log('Running @ ' + app.get('port')));
 app.get('/', function(req, res) {
 	res.set('Content-Type', 'text/html')
 	.sendFile(path.join(__dirname, '/client/build/index.html'))
-});
-
-// GitHub push webhook
-app.post("/webhooks", function (req, res) {
-	const sender = req.body.sender;
-	const branch = req.body.ref;
-	const githubUsername = 'jeth318';
-
-	if(branch.indexOf('master') > -1 && sender.login === githubUsername){
-		res.sendStatus(200);
-		runDeployment(res);
-	}
 });
