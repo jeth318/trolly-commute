84f537aad84f4416ed85a3b8899da3676fcda078
Jesper Thörnberg <jesper.thornberg@me.com>
2019-12-15 15:23:46 +0100

    Improve token url

--- client/src/resources/rest.config.js
+++ client/src/resources/rest.config.js
@@ -2,10 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 const PROTOCOL = document.location.protocol;
 const HOSTNAME = document.location.hostname;
+const isLocalhost = HOSTNAME === 'localhost'; 
 
-console.log(`${PROTOCOL}//${HOSTNAME}:3001/api/token`);
-
-export const tripBaseUrl = 'https://api.vasttrafik.se/bin/rest.exe/v2/trip?originId=';
-export const tokenUrl = `${PROTOCOL}//${HOSTNAME}:3001/api/token`;
 export const allStopsUrl = 'https://api.vasttrafik.se/bin/rest.exe/v2/location.allstops?format=json';
 export const locationBaseUrl = 'https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=';
+export const tripBaseUrl = 'https://api.vasttrafik.se/bin/rest.exe/v2/trip?originId=';
+export const tokenUrl = isLocalhost ? '/api/token' : `${PROTOCOL}//${HOSTNAME}/api/token`;
