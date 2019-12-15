3453e4666bcfa6f43743e6a1f0b1ae4b6b77e149
Jesper Thörnberg <jesper.thornberg@me.com>
2019-12-15 15:15:04 +0100

    Fix protocol and host variabeles

--- client/src/resources/rest.config.js
+++ client/src/resources/rest.config.js
@@ -1,7 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
+const PROTOCOL = document.location.protocol;
+const HOSTNAME = document.location.hostname;
+
+console.log(`${PROTOCOL}//${HOSTNAME}:3001/api/token`);
 
 export const tripBaseUrl = 'https://api.vasttrafik.se/bin/rest.exe/v2/trip?originId=';
-export const tokenUrl = 'http://127.0.0.1:3001/api/token';
+export const tokenUrl = `${PROTOCOL}//${HOSTNAME}:3001/api/token`;
 export const allStopsUrl = 'https://api.vasttrafik.se/bin/rest.exe/v2/location.allstops?format=json';
 export const locationBaseUrl = 'https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=';
